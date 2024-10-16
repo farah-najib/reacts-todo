53a982b8e4a75d9d8787d05a09dd15d8b64f3c29
Farah Najib <farahengnajib@gmail.com>
2024-10-16 12:08:40 +0200

    change the nav name

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,19 +5,23 @@ import { NavLink, Link } from 'react-router-dom'
 
 const Navigation = () => {
 	return (
-		<Navbar bg="dark" variant="dark" expand="md">
-			<Container>
-				<Navbar.Brand as={Link} to="/">📝 React Todos</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" expand="md">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    📝 React Typescript Todos
+                </Navbar.Brand>
 
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/todos">Todos</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	)
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={NavLink} end to="/todos">
+                            Todos
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default Navigation
