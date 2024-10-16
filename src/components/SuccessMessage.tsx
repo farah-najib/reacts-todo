7946d1f7588fc198c62e7b74f0fcc3c61f101377
Farah Najib <farahengnajib@gmail.com>
2024-10-16 12:07:31 +0200

    add comments to more understand the code

--- src/components/SuccessMessage.tsx
+++ src/components/SuccessMessage.tsx
@@ -1,17 +1,19 @@
 import Alert from "react-bootstrap/Alert";
 
 interface SuccessMessageProps {
-	children: React.ReactNode;
-	heading?: string;
+    children: React.ReactNode //React.ReactNode, which is a common type that can be anything React can render.
+    heading?: string //heading?: The ? means this property is optional. It will be a string if provided, but it doesn’t have to be included when using the component. If included, it’s used to show a heading at the top of the message.
 }
-
+//React.FC is the type definition for functional components (a function that returns JSX). in TypeScript
+//const SuccessMessage = ({ children, heading }: SuccessMessageProps) =>  "This is also a valid way to type the props."
 const SuccessMessage: React.FC<SuccessMessageProps> = ({ children, heading }) => {
 	return (
-		<Alert variant="success">
-			{heading && <Alert.Heading>{heading}</Alert.Heading>}
-			{children}
-		</Alert>
-	)
+        <Alert variant="success">
+            {heading && <Alert.Heading>{heading}</Alert.Heading>}
+            {/* If heading exists, it renders the Alert.Heading component with the heading text inside it */}
+            {children}
+        </Alert>
+    )
 }
 
 export default SuccessMessage;
