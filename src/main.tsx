0ac0358815e70d50d815bd3e0990abbbfac08b14
Farah Najib <farahengnajib@gmail.com>
2024-10-22 09:34:08 +0200

    modify main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import ThemeContextProvider from './contexts/ThemeContext.tsx'
 import App from './App.tsx'
+
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 5 * 60 * 1000, // 5 minutes
+            gcTime: 15 * 60 * 1000 // 15 minutes
+        }
+    }
+})
+
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ThemeContextProvider>
+                    <App />
+                </ThemeContextProvider>
+            </BrowserRouter>
+        </QueryClientProvider>
     </StrictMode>
 )
