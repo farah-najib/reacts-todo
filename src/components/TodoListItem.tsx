680696bc9b41151a25f54af2790605393e00b0d6
Farah Najib <farahengnajib@gmail.com>
2024-10-22 09:33:37 +0200

    modify TodoListItem

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -3,29 +3,65 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { Todo } from "../services/TodoAPI.types";
 
 interface TodoListItemProps {
-	onToggle: (todo: Todo) => Promise<void>;
-	todo: Todo;
+    onDelete: (todo: Todo) => Promise<void>
+    onEdit: (todo: Todo, newTitle: string) => Promise<void>
+    onToggle: (todo: Todo) => Promise<void>
+    todo: Todo
 }
 
-const TodoListItem: React.FC<TodoListItemProps> = ({ onToggle, todo }) => {
-	const handleToggleTodo = () => {
-		onToggle(todo);
-	}
+const TodoListItem: React.FC<TodoListItemProps> = ({
+    onDelete,
+    onEdit,
+    onToggle,
+    todo
+}) => {
 
-	return (
-		<ListGroup.Item
-			className={todo.completed ? "todo done" : "todo"}
-		>
-			<span className="todo-title">
-				{todo.title}
-			</span>
-			<div className="todo-actions">
-				<Button onClick={handleToggleTodo} variant="secondary">Toggle</Button>
-				<Button variant="warning">Edit</Button>
-				<Button variant="danger">Delete</Button>
-			</div>
-		</ListGroup.Item>
-	)
+	const handleDeleteTodo = () => {
+        // Ask user to confirm delete
+        if (!confirm('U SURE BRO?!')) {
+            return
+        }
+
+        // Tell parent to delete the todo
+        onDelete(todo)
+    }
+
+    const handleEditTodo = () => {
+        const newTitle = prompt('Enter the new title', todo.title)
+
+        // Ask user what the new title should be
+        if (!newTitle) {
+            return
+        }
+
+        // Tell parent to update the todo
+        onEdit(todo, newTitle)
+    }
+
+
+
+
+
+    const handleToggleTodo = () => {
+        onToggle(todo)
+    }
+
+    return (
+        <ListGroup.Item className={todo.completed ? 'todo done' : 'todo'}>
+            <span className="todo-title">{todo.title}</span>
+            <div className="todo-actions">
+                <Button onClick={handleToggleTodo} variant="secondary">
+                    Toggle
+                </Button>
+                <Button onClick={handleEditTodo} variant="warning">
+                    Edit
+                </Button>
+                <Button onClick={handleDeleteTodo} variant="danger">
+                    Delete
+                </Button>
+            </div>
+        </ListGroup.Item>
+    )
 }
 
 export default TodoListItem;
