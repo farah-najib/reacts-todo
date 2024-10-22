e7aba37984a27c60a41f45ede296bc398c85e450
Farah Najib <farahengnajib@gmail.com>
2024-10-22 09:32:52 +0200

    add theme button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import Badge from 'react-bootstrap/Badge'
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { NavLink, Link } from 'react-router-dom'
-
+import useTheme from '../hooks/useTheme'
+import TodosAPI from '../services/TodoAPI'
 const Navigation = () => {
-	return (
+    const { data: todos, isSuccess } = useQuery({
+        queryKey: ['todos'],
+        queryFn: TodosAPI.getTodos
+    })
+    const { isDarkMode, toggleTheme } = useTheme()
+    return (
         <Navbar bg="dark" variant="dark" expand="md">
             <Container>
                 <Navbar.Brand as={Link} to="/">
@@ -15,10 +24,16 @@ const Navigation = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link as={NavLink} end to="/todos">
-                            Todos
+                            Todos{' '}
+                            {isSuccess && (
+                                <Badge bg="primary">{todos.length}</Badge>
+                            )}
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
+                <Button variant="outline-secondary" onClick={toggleTheme}>
+                    {isDarkMode ? '☀️' : '🌙'}
+                </Button>
             </Container>
         </Navbar>
     )
