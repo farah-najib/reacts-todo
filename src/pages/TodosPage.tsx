0f3810d1e97c7df58edfa1ab9eec8004b32b767d
Farah Najib <farahengnajib@gmail.com>
2024-10-22 09:36:06 +0200

    modify TodosPage to use react query

--- src/pages/TodosPage.tsx
+++ src/pages/TodosPage.tsx
@@ -1,60 +1,99 @@
-import { useEffect, useState } from 'react'
+//import { useEffect, useState } from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import PacmanLoader from 'react-spinners/PacmanLoader'
 import TodoListItem from '../components/TodoListItem'
 import TodoAPI from '../services/TodoAPI'
-import { Todo } from '../services/TodoAPI.types'
-import SuccessMessage from '../components/SuccessMessage'
+import { CreateTodoPayload, Todo } from '../services/TodoAPI.types'
+import AddTodoForm from '../components/AddTodoForm'
+import { useQuery } from '@tanstack/react-query'
+//import SuccessMessage from '../components/SuccessMessage'
 
 const TodosPage = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [todos, setTodos] = useState<Todo[] | null>(null);
+	//const [isLoading, setIsLoading] = useState(true);
+	//const [todos, setTodos] = useState<Todo[] | null>(null);
 
-	const getTodos = async () => {
-		// reset initial state
-		setIsLoading(true);
+	// const getTodos = async () => {
+	// 	// reset initial state
+	// 	setIsLoading(true);
 
-		const data = await TodoAPI.getTodos();
-		setIsLoading(false);
-		setTodos(data);
-	}
+	// 	const data = await TodoAPI.getTodos();
+	// 	setIsLoading(false);
+	// 	setTodos(data);
+	// }
+    const {
+        data: todos,
+        isLoading,
+        refetch
+    } = useQuery({
+        queryKey: ['todos'],
+        queryFn: TodoAPI.getTodos
+    })
+
+    const createTodo = async (data: CreateTodoPayload) => {
+        await TodoAPI.createTodo(data)
+
+        // Re-fetch all todos
+        refetch()
+    }
+
+    const deleteTodo = async (todo: Todo) => {
+        await TodoAPI.deleteTodo(todo.id)
+
+        // Re-fetch all todos
+        refetch()
+    }
+
+    const editTodo = async (todo: Todo, newTitle: string) => {
+        await TodoAPI.updateTodo(todo.id, { title: newTitle })
+
+        // Re-fetch all todos
+        refetch()
+    }
 
 	const toggleTodo = async (todo: Todo) => {
-		await TodoAPI.updateTodo(todo.id, { completed: !todo.completed });
+        await TodoAPI.updateTodo(todo.id, { completed: !todo.completed })
 
-		// Re-fetch all todos
-		getTodos();
-	}
+        refetch()
+        // Re-fetch all todos
+        //getTodos();
+    }
 
 	// Fetch todos when component is being mounted
-	useEffect(() => {
-		getTodos();
-	}, []);
+	// useEffect(() => {
+	// 	getTodos();
+	// }, []);
 
 	return (
-		<>
-			<h1 className="mb-3">Todos</h1>
+        <>
+            <h1 className="mb-3">Todos</h1>
+            <AddTodoForm onAddTodo={createTodo} />
 
-			{/* Form should validate that a title is entered and at least 2 chars long, ONLY then should the parent's function for creating the todo be called */}
-			{/* <AddTodoForm onAddTodo={createTodo} /> */}
+            {/* Form should validate that a title is entered and at least 2 chars long, ONLY then should the parent's function for creating the todo be called */}
+            {/* <AddTodoForm onAddTodo={createTodo} /> */}
 
-			<SuccessMessage heading="success Message">
+            {/*<SuccessMessage heading="success Message">
 				<p>you can add note</p>
-			</SuccessMessage>
+			</SuccessMessage>*/}
 
-			{isLoading && <PacmanLoader size={30} color="#f00" speedMultiplier={1.25} />}
+            {isLoading && (
+                <PacmanLoader size={30} color="#f00" speedMultiplier={1.25} />
+            )}
 
-			{todos && <ListGroup className="todolist">
-				{todos.map(todo => (
-					<TodoListItem
-						key={todo.id}
-						onToggle={toggleTodo}
-						todo={todo}
-					/>
-				))}
-			</ListGroup>}
-		</>
-	)
+            {todos && (
+                <ListGroup className="todolist">
+                    {todos.map((todo) => (
+                        <TodoListItem
+                            key={todo.id}
+                            onDelete={deleteTodo}
+                            onEdit={editTodo}
+                            onToggle={toggleTodo}
+                            todo={todo}
+                        />
+                    ))}
+                </ListGroup>
+            )}
+        </>
+    )
 }
 
 export default TodosPage
